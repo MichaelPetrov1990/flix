b847348a666a48547ad2fe2c0116180ba5eaf78f
MichaelPetrov1990 <mpetrovcello@gmail.com>
2024-11-19 17:05:09 +0000

    add helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_signin
+    unless current_user
+      session[:intended_url] = request.url
+      redirect_to new_session_path, notice: "Please sign in to continue"
+    end
+  end
+
+  helper_method :current_user
+
+  def current_user?(user)
+    current_user == user
+  end
+
+  helper_method :current_user?
 end
