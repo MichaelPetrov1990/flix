0a0445979b59ed3655705e64df5021af9ed9c4d6
MichaelPetrov1990 <mpetrovcello@gmail.com>
2024-11-19 11:18:50 +0000

    sign in and sign out on creation and deletion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = user.id
       redirect_to @user, notice: "User successfully created!"
     else
       render :new, status: :unprocessable_entity
@@ -36,6 +37,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
+    session[:user_id] = nil
     redirect_to movies_url, status: :see_other,
     alert: "Account successfully deleted!"
   end
