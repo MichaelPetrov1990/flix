6ced8b783a73e1064d51a1e6e72b8ca52d3852b9
MichaelPetrov1990 <mpetrovcello@gmail.com>
2024-11-28 15:57:18 +0000

    add attachment and validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,15 +7,12 @@ class Movie < ApplicationRecord
   has_many :fans, through: :favorites, source: :user
   has_many :characterizations, dependent: :destroy
   has_many :genres, through: :characterizations
+  has_one_attached :main_image, dependent: :destroy
 
   validates :title, :released_on, :duration, presence: :true
   validates :title, presence: true, uniqueness: true
   validates :description, length: { minimum: 25}
-
-  validates :image_file_name, format: {
-    with: /\w+\.(jpg|png)\z/i,
-    message: "must be a JPG or PNG image"
-  }
+  validate :acceptable_image
 
   RATINGS = %w(G PG PG-13 R NC-17)
 
@@ -48,6 +45,19 @@ class Movie < ApplicationRecord
 
   private
 
+  def acceptable_image
+    return unless main_image.attached?
+  
+    unless main_image.blob.byte_size <= 1.megabyte
+      errors.add(:main_image, "is too big")
+    end
+  
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(main_image.blob.content_type)
+      errors.add(:main_image, "must be a JPEG or PNG")
+    end
+  end
+  
   def set_slug
     self.slug = title.parameterize
   end
